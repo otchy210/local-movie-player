5664f175926b63f026f4d28ffe44cef74f54bbee
Otchy <id+github@indeed.com>
2020-08-16 01:31:30 +0900

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Main = () => {
+    return <main>main</main>;
+};
+
+ReactDom.render(<Main/>, document.getElementById('react'));
