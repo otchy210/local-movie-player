177bb9a3bf6af6fb4fac84f69a8a00295c5c9a41
Otchy <id+github@indeed.com>
2020-10-03 00:53:37 +0900

    Add close button, double click feature, expand mode, and showing time feature

--- src/js/components/Player.js
+++ src/js/components/Player.js
@@ -2,6 +2,15 @@ import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { unit1, unit3 } from './styled/common';
 
+const SIZE_S = {label: '小', width: '12.5%'};
+const SIZE_M = {label: '中', width: '16.666%'};
+const SIZE_L = {label: '大', width: '25%'};
+const SIZES = [SIZE_S, SIZE_M, SIZE_L];
+
+const MODE_R = {label: '標準'};
+const MODE_X = {label: '拡大'};
+const MODES = [MODE_R, MODE_X];
+
 const Bg = styled.div`
     position: fixed;
     width: 100vw;
@@ -17,9 +26,9 @@ const Panel = styled.div`
     position: fixed;
     display: flex;
     flex-direction: column;
-    width: 90vw;
-    max-width: 800px;
-    height: 90vh;
+    width: ${props => props.mode === MODE_X ? '96vw' : '90vw'};
+    max-width: ${props => props.mode === MODE_X ? 'none' : '800px'};
+    height: ${props => props.mode === MODE_X ? '96vh' : '90vh'};
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
@@ -31,6 +40,8 @@ const Panel = styled.div`
 
 const Movie = styled.video.attrs({controls: 'controls'})`
     width: 100%;
+    max-height: ${props => props.mode === MODE_X ? 'calc(96vh - 150px)' : 'none'};
+    background-color: #000;
 `;
 
 const MovieWrapper = styled.div`
@@ -43,35 +54,98 @@ const Controls = styled.div`
     margin-top: ${unit1};
 `;
 
+const ButtonGroup = styled.div`
+    display: inline-block;
+    margin-right: ${unit1};
+    & > button:first-child {
+        border-radius: ${unit1} 0 0 ${unit1};
+    }
+    & > button:last-child {
+        border-radius: 0 ${unit1} ${unit1} 0;
+    }
+`;
+
 const Button = styled.button`
-    width: ${unit3};
     line-height: ${unit3};
     text-align: center;
-    margin-right: 2px;
-    background-color: #333;
+    margin-right: 1px;
+    background-color: ${props => props.selected ? '#3c3' : '#333'};
     border: none;
-    border-radius: 2px;
     color: #fff;
     cursor: pointer;
     outline: none;
 `;
 
+const Close = styled.button`
+    position: absolute;
+    right: 0;
+    top: 0;
+    line-height: ${unit3};
+    text-align: center;
+    background-color: #900;
+    border: 4px solid #fff;
+    border-radius: 50%;
+    color: #fff;
+    cursor: pointer;
+    outline: none;
+    transform: translate(30%, -30%);
+`;
+
 const Thumbnails = styled.div`
     margin-top: ${unit1};
     overflow: scroll;
+    white-space: ${props => props.mode === MODE_X ? 'nowrap' : 'none'};
+`;
+
+const ThumbnailWrapper = styled.span`
+    display: inline-block;
+    position: relative;
+    width: ${props => props.mode === MODE_X ? '120px' : `calc(${props.size.width} - 4px)`};
+    border-style: solid;
+    border-width: 2px;
+    border-color: ${props => props.focused ? '#3c3' : 'rgba(0,0,0,0)'};
+    cursor: pointer;
+    &:hover > span {
+        display: inline-block;
+    }
+`;
+
+const Time = styled.span`
+    display: none;
+    position: absolute;
+    left: 4px;
+    top: 2px;
+    padding: 2px 4px;
+    border-radius: 4px;
+    border: 1px solid rgba(255,255,255,0.3);
+    background-color: rgba(0,0,0,0.5);
+    color: rgba(255,255,255,0.7);
+    font-size: 0.7rem;
 `;
 
 const Thumbnail = styled.img`
-    width: calc(${props => props.size} - 4px);
-    cursor: pointer;
-    border-style: solid;
-    border-width: 2px;
-    border-color: ${props => props.focused ? '#0f0' : 'rgba(0,0,0,0)'};
+    width: 100%;
 `;
+
+const formatTime = (time) => {
+    const hours = parseInt(time / 3600);
+    const mins = parseInt( (time - hours * 3600) / 60 );
+    const secs = time - hours * 3600 - mins * 60;
+    if (hours > 0) {
+        const hoursStr = hours.toString().padStart(2, '0');
+        const minsStr = mins.toString().padStart(2, '0');
+        const secsStr = secs.toString().padStart(2, '0');
+        return [hoursStr, minsStr, secsStr].join(':');
+    }
+    const minsStr = mins.toString();
+    const secsStr = secs.toString().padStart(2, '0');
+    return [minsStr, secsStr].join(':');
+}
 
 const Player = (props) => {
     const { movie, unselectMovie } = props;
-    const [ size, setSize ] = useState('12.5%');
+    const [ size, setSize ] = useState(SIZE_S);
+    const [ mode, setMode ] = useState(MODE_R);
     const [ focusedIndex, setFocusedIndex ] = useState(0);
     const ref = useRef();
     const context = globalThis.context;
@@ -87,17 +161,30 @@ const Player = (props) => {
     }, []);
     return <>
         <Bg onClick={unselectMovie}></Bg>
-        <Panel>
+        <Panel mode={mode}>
+            <Close onClick={unselectMovie}>閉</Close>
             <Name>{movie.name}</Name>
-            <MovieWrapper><Movie src={url} ref={ref}></Movie></MovieWrapper>
+            <MovieWrapper><Movie src={url} ref={ref} mode={mode} /></MovieWrapper>
             <Controls>
-                <Button onClick={() => {setSize('12.5%')}}>小</Button>
-                <Button onClick={() => {setSize('16.66%')}}>中</Button>
-                <Button onClick={() => {setSize('25%')}}>大</Button>
+                <ButtonGroup>{
+                    SIZES.map(s => <Button onClick={() => {setSize(s)}} selected={size === s}>{s.label}</Button>)
+                }</ButtonGroup>
+                <ButtonGroup>{
+                    MODES.map(m => <Button onClick={() => {setMode(m)}} selected={mode === m}>{m.label}</Button>)
+                }</ButtonGroup>
             </Controls>
-            <Thumbnails>
+            <Thumbnails mode={mode}>
                 {movie.thumbnails.map((data, i) => {
-                    return <Thumbnail src={data} size={size} onClick={()=>{ref.current.currentTime = i * 30}} focused={focusedIndex === i}/>
+                    return <ThumbnailWrapper
+                        mode={mode}
+                        size={size}
+                        focused={focusedIndex === i}
+                        onClick={()=>{ref.current.currentTime = i * 30}}
+                        onDoubleClick={()=>{ref.current.play()}}
+                    >
+                        <Time>{formatTime(i*30)}</Time>
+                        <Thumbnail src={data} />
+                    </ThumbnailWrapper>
                 })}
             </Thumbnails>
         </Panel>
